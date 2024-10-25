b9405ede2cb9a42f92647bfe3111cee17cbb66a9
Santiago Gómez <sgomez975@soyudemedellin.edu.co>
2024-10-25 07:19:44 -0500

    Creación de tarjetas

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import data from './data/pokemon.json' assert { type: 'json' };
+import data from './data/pokemon.json' with { type: 'json' };
 
 const POKEMON_TYPES_ICONS = {
   fire: 'bxs-hot',
@@ -17,3 +17,30 @@ const POKEMON_TYPES_ICONS = {
 
 console.log(data)
 // Tu código aquí
+const cardsContainer = document.querySelector(".cards")
+
+function getCardsHtml(pokemonData) {
+  const icon = POKEMON_TYPES_ICONS [pokemonData.type[0]]
+  const cardHtml = `
+  <div class="pokemon-card ${pokemonData.type[0]}">
+        <div class="pokemon-card__type-label ${pokemonData.type[0]}">
+          <i class="pokemon-card__name-icon bx ${icon} ${pokemonData.type[0]}"></i>
+          <span>${pokemonData.type[0]}</span>
+        </div>
+        <div class="pokemon-card__image">
+          <img src="${pokemonData.img}" loading="lazy">
+        </div>
+        <div class="pokemon-card__name">
+          <h4>${pokemonData.name}</h4>
+        </div>
+        <p class="pokemon-card__eyebrow">${pokemonData.num}</p>
+      </div>
+    `
+  return cardHtml
+}
+const cardsHtml = data.pokemon.map((pokemonData) => {
+  return getCardsHtml(pokemonData)
+}).join("")
+
+
+cardsContainer.innerHTML = cardsHtml
